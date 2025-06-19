de93504efaf2debfd6a0a2c190934f6daf72f627
Towble <info@towble.com>
2025-06-19 01:00:13 -0700

    Create index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import TowRequestForm from '../components/TowRequestForm';
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-gray-100 p-4">
+      <TowRequestForm />
+    </div>
+  );
+}
